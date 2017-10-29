a2947d1decbc93b8a5e6fec870de7ae886809081
travelgeezer <travelgeezer@icloud.com>
2017-10-29 20:07:54 +0800

    [T] response: try use jsonify return data

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route('/api/sample-list', methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def sample_list():
     if request.method == 'GET':
-        return 'send request with `GET` method'
+        data = {
+            'code': 0,
+            'data': [{'sample': 'sample data', 'list': [1, 2, 3]}],
+            'info': 'ok'
+        }
+        return jsonify(data)
     elif request.method == 'POST':
         return 'send request with `POST` method'
     elif request.method == 'PUT':
