1902ca23a9e368562e59d40182337f6fc2b690d4
travelgeezer <travelgeezer@icloud.com>
2017-11-08 11:54:38 +0800

    [T] factory: add json resonse to app

--- factory.py
+++ factory.py
@@ -5,16 +5,18 @@ from flask import Flask
 from flask.views import MethodView
 from flask_mongoengine import MongoEngine
 from flask_cors import CORS
-
+from .json_response import JSONResponse
 db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
 
+    app.response_class = JSONResponse
+
     app.config.from_object('sasukekun_flask.config')
 
     db.init_app(app)
 
+    CORS(app)
 
-    print(CORS(app))
     return app
