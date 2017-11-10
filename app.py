f9b81d142460475eb3a6af75c3b4770c3d1a4be6
travelgeezer <travelgeezer@icloud.com>
2017-11-10 17:34:01 +0800

    [T] app: add upload to Blueprint

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 from .factory import create_app
 from .blog.views import blog
-
+from .upload.views import upload
 
 app = create_app()
 app.register_blueprint(blog)
+app.register_blueprint(upload)
 
 @app.route('/')
 def hello_rest():
