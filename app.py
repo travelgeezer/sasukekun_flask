11d2ed9151d193712681fe2527d74174894f7d74
travelgeezer <travelgeezer@icloud.com>
2017-11-10 10:01:01 +0800

    [T] app: delete unused dependencies

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
-import click
-from flask import request, jsonify
-from flask.views import MethodView
-from flask_cors import CORS
 from .factory import create_app
 from .blog.views import blog
 
@@ -12,15 +7,6 @@ from .blog.views import blog
 app = create_app()
 app.register_blueprint(blog)
 
-
-@app.cli.command()
-def initdb():
-    click.echo('init the db')
-
 @app.route('/')
 def hello_rest():
     return 'hello flask RESTful api'
-
-@app.route('/webAPI/test')
-def test():
-    return 'test web api'
