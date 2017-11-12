f918dd48b23199920350d07f70326af17aecaecc
travelgeezer <travelgeezer@icloud.com>
2017-11-12 22:22:59 +0800

    [T] local: add settings

---
+++ local_settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+MONGODB_SETTINGS = {
+    'db': 'sasukekun-flask',
+    'host': 'localhost',
+    'port': 27017
+}
+
+API_WEB = '/webAPI/'
+API_IMAGE = '/images/'
+
+UPLOAD_FOLDER = '/tmp/permdir'
